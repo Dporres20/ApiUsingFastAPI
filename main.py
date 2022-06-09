db58411a10c0499771632decfb9175cabb4af3b3
Diego Porres <dporres20@gmail.com>
2022-06-09 16:36:33 -0400

    Added delete path to API

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
+from http.client import HTTPException
 from typing import List
-from uuid import uuid4
-from fastapi import FastAPI
+from uuid import UUID, uuid4
+from fastapi import FastAPI, HTTPException
 from models import User, Gender, Role
 
 app = FastAPI()
@@ -48,3 +49,15 @@ async def fetch_users():
 async def register_user(user:User):
     db.append(user)
     return {"id": user.id}
+
+#Delete user
+@app.delete("/api/v1/users/{user_id}")
+async def delete_user(user_id: UUID):
+    for user in db:
+        if user.id == user_id:
+            db.remove(user)
+            return
+    raise HTTPException(
+        status_code = 404,
+        detail = f"User with ID: {user_id} does not exist"
+    )
